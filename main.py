6e6ebf58d1cf24ebc67317211d83b696c1ceb181
BrunoPM2001 <brunoporras2001@gmail.com>
2024-04-26 11:04:48 -0500

    Adding handle of exceptions

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ soup = BeautifulSoup(res2.text, 'html.parser')
 div = soup.find('div', { 'id' : 'resultado_busqueda' })
 datos = div.find_all('p', recursive=True)
 
-bs_nombre = BeautifulSoup(str(datos[2]), 'html.parser')
-p_nombre = bs_nombre.find('p')
-nombre = ''.join(p_nombre.find_all(string=True, recursive=False))
+try:
+    
+  bs_nombre = BeautifulSoup(str(datos[2]), 'html.parser')
+  p_nombre = bs_nombre.find('p')
+  nombre = ''.join(p_nombre.find_all(string=True, recursive=False))
 
-bs_paterno = BeautifulSoup(str(datos[3]), 'html.parser')
-p_paterno = bs_paterno.find('p')
-paterno = ''.join(p_paterno.find_all(string=True, recursive=False))
+  bs_paterno = BeautifulSoup(str(datos[3]), 'html.parser')
+  p_paterno = bs_paterno.find('p')
+  paterno = ''.join(p_paterno.find_all(string=True, recursive=False))
 
-bs_materno = BeautifulSoup(str(datos[4]), 'html.parser')
-p_materno = bs_materno.find('p')
-materno = ''.join(p_materno.find_all(string=True, recursive=False))
+  bs_materno = BeautifulSoup(str(datos[4]), 'html.parser')
+  p_materno = bs_materno.find('p')
+  materno = ''.join(p_materno.find_all(string=True, recursive=False))
 
-print("Nombres: ", nombre.strip())
-print("Apellido paterno: ", paterno.strip())
-print("Apellido materno: ", materno.strip())
+  print("Nombres: ", nombre.strip())
+  print("Apellido paterno: ", paterno.strip())
+  print("Apellido materno: ", materno.strip())
+except:
+  print("Error:", datos)
